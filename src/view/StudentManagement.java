2486344f12c562d7c7519369e67089568f9784b4
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-10-04 10:45:58 +0700

    add

--- src/view/StudentManagement.java
+++ src/view/StudentManagement.java
@@ -5,12 +5,76 @@
  */
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import model.Student;
+
 /**
  *
  * @author Phucdz
  */
 public class StudentManagement {
-	public static void main(String[] args) {
-		System.out.println("phuc dep trai");
+	public static void InsertStudent() throws Exception {
+		Student student = new Student();
+		student.InputInfomation();
+		Scanner input = new Scanner(System.in);
+		do {
+			if(controller.Manager.getStudentByID(student.getID()) != null) {
+				System.out.println("Id IS EXIST, PLEASE INPUT AGAIN!!!");
+				student.setID(input.next());
+			} else {
+				break;
+			}
+		} while(true);
+		int result = controller.Manager.InsertStudent(student);
+		if(result == 0) {
+			System.out.println("ID IS EXIST");
+		} else {
+			System.out.println("Insert Student Success...");
+		}
+	}
+	public static void ShowStudent() throws Exception{
+		List<Student> listStudents = controller.Manager.getStudentList();
+		for(int i = 0; i < listStudents.size(); i++) {
+			System.out.println("Student: " + (i+1));
+			System.out.println("ID: " + listStudents.get(i).getID());
+			System.out.println("Name: " + listStudents.get(i).getName());
+			System.out.println("Email: " + listStudents.get(i).getEmail());
+			System.out.println("GPA: "+ listStudents.get(i).getGPA());
+		}
+		
+	}
+	public static void main(String[] args) throws Exception{
+		String choose = null;
+		boolean exit = true;
+		Scanner input = new Scanner(System.in);
+		showmenu();
+		while(exit) {
+			choose = input.next();
+			switch(choose) {
+				case "1" :
+					InsertStudent();
+					break;
+				case "2" :
+					ShowStudent();
+					break;
+				case "3" :
+					exit = false;
+					break;
+				default: 
+					System.out.println("Your InPut is Error");
+					break;
+					
+			}
+			showmenu();
+		}
+		
+	}
+	public static void showmenu() {
+		System.out.println("1. Insert Student");
+		System.out.println("2. Show All Student");
+//		System.out.println("3. Search Student By ID");
+		System.out.println("3. Exit");
 	}
 }
