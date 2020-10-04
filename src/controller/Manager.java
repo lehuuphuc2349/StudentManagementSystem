97c7e169f090075b905018ac5fc27c4a9bf206ac
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-10-04 08:58:52 +0700

    add view

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jdbcmanagement;
+package controller;
 
 /**
  *
  * @author Phucdz
  */
-public class JDBCManagement {
-
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
-	}
+public class Manager {
 	
 }
---
+++ src/view/StudentManagement.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author Phucdz
+ */
+public class StudentManagement {
+	public static void main(String[] args) {
+		System.out.println("phuc dep trai");
+	}
+}
